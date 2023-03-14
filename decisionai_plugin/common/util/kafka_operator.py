63c27d9d275344c3fc2e40dfefe71bced5153165
Chunlei Wang <chuwan@microsoft.com>
2023-03-14 21:57:20 +0800

    Remove comment

--- decisionai_plugin/common/util/kafka_operator.py
+++ decisionai_plugin/common/util/kafka_operator.py
@@ -103,7 +103,7 @@ def consume_loop(process_func, topic, retry_limit=0, error_callback=None, config
             kafka_configs = get_kafka_configs()
             if config is not None:
                 kafka_configs.update(config)
-            #log.info("kafka configs: " + json.dumps(kafka_configs))
+                
             consumer = KafkaConsumer(topic, **{**kafka_configs,
                                                'group_id': 'job-controller-v2-%s' % topic,
                                                'value_deserializer': lambda m: json.loads(m.decode('utf-8')),
